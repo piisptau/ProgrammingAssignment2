6fc497651f327701fe78850b8fcbcb9275ce0acb
Tauno Piispanen <piispanentauno7@gmail.com>
2014-07-22 16:09:28 +0300

    Initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cachesolve functions can be used speed up calculation of inverse matrices
+## if the inverse matrix has already been calculated the cached result is used
+## 
 
-## Write a short comment describing this function
+## makeCacheMatrix function creates a new cache matrix matrix object from matrix parameter, fro example
+##
+##   m1 <- matrix(c(78,56,45,23),nrow=2,ncol=2)
+##   cm <- makeCacheMatrix(m1)
+##
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve<- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cachesolve function calculates inverse matrix for a cache matrix object parameter
+## if the inverse matrix has already been calculated, the cached result is returned
+##
+## Example:
+##
+##    m1 <- matrix(c(78,56,45,23),nrow=2,ncol=2)
+##    cm <- makeCacheMatrix(m1)
+##
+##    cachesolve(cm)
+##            [,1]        [,2]
+## [1,] -0.03168044  0.06198347
+## [2,]  0.07713499 -0.10743802
+##
+##    cachesolve(cm)
+## getting cached data
+##            [,1]        [,2]
+## [1,] -0.03168044  0.06198347
+## [2,]  0.07713499 -0.10743802
+##
+
+cachesolve <- function(x, ...) {
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
+
+
